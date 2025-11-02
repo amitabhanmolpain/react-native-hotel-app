543e4521e59b7b348a6e9e67f13641b4b1374b4f
Amitabh Anmol Pain <ami05tabh@gmail.com>
2025-11-02 15:20:14 +0530

    some-fix

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'http://192.168.0.107:54321'
-const SUPABASE_ANON_KEY = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
+// Environment variables
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
+// Create Supabase client
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
