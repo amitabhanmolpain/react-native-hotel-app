f374f658d6b3912ded051388f94b911e12106c3d
Amitabh Anmol Pain <ami05tabh@gmail.com>
2025-11-13 14:18:54 +0530

    somee

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'http://192.168.0.107:54321'
+const SUPABASE_URL = 'http://192.168.0.103:54321'
 const SUPABASE_ANON_KEY = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)  
